085159c4da62173560355c01c21b2ca50cdfeb57
Bruno Barbosa <brb.dev@outlook.com>
2022-01-04 19:55:09 -0400

    adiciona model conta

---
+++ src/main/java/com/ALTbruno/DesafioPubFuture/model/Conta.java
@@ -0,0 +1,85 @@
+package com.ALTbruno.DesafioPubFuture.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_CONTAS")
+public class Conta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idConta;
+
+	@NotNull
+	@Size(max = 5)
+	private String agenciaConta;
+
+	@Size(min = 5, max = 15)
+	private String numeroConta;
+
+	@NotNull
+	@Size(max = 5)
+	private String tipoConta;
+
+	@NotNull
+	@Size(max = 5)
+	private BigDecimal saldoConta;
+
+	@NotNull
+	@Size(min = 8, max = 8)
+	private String instituicaoFinanceira;
+
+
+	public Integer getIdConta() {
+		return idConta;
+	}
+
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
+
+	public String getAgenciaConta() {
+		return agenciaConta;
+	}
+
+	public void setAgenciaConta(String agenciaConta) {
+		this.agenciaConta = agenciaConta;
+	}
+
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public String getTipoConta() {
+		return tipoConta;
+	}
+
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+
+	public BigDecimal getSaldoConta() {
+		return saldoConta;
+	}
+
+	public void setSaldoConta(BigDecimal saldoConta) {
+		this.saldoConta = saldoConta;
+	}
+
+	public String getInstituicaoFinanceira() {
+		return instituicaoFinanceira;
+	}
+
+	public void setInstituicaoFinanceira(String instituicaoFinanceira) {
+		this.instituicaoFinanceira = instituicaoFinanceira;
+	}
+
+}
