d197381acc0d4ac58109a26a42f28a5ff81dc1a6
Bruno Barbosa <brb.dev@outlook.com>
2022-01-04 20:07:35 -0400

    adiciona model receita

---
+++ src/main/java/com/ALTbruno/DesafioPubFuture/model/Receita.java
@@ -0,0 +1,96 @@
+package com.ALTbruno.DesafioPubFuture.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "TB_RECEITAS")
+public class Receita {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idReceita;
+
+	@NotNull
+	@DecimalMin(value = "0")
+	private BigDecimal valorReceita;
+
+	@NotNull
+	private Date dataRecebimento;
+
+	@NotNull
+	private Date dataRecebimentoEsperado;
+
+	@NotNull
+	@Size(max = 10)
+	private String tipoReceita;
+
+	@NotNull
+	@Size(max = 255)
+	private String descricaoReceita;
+
+	@ManyToOne
+	@JoinColumn(name = "id_conta")
+	private Conta conta;
+
+
+	public Integer getIdReceita() {
+		return idReceita;
+	}
+
+	public void setIdReceita(Integer idReceita) {
+		this.idReceita = idReceita;
+	}
+
+	public BigDecimal getValorReceita() {
+		return valorReceita;
+	}
+
+	public void setValorReceita(BigDecimal valorReceita) {
+		this.valorReceita = valorReceita;
+	}
+
+	public Date getDataRecebimento() {
+		return dataRecebimento;
+	}
+
+	public void setDataRecebimento(Date dataRecebimento) {
+		this.dataRecebimento = dataRecebimento;
+	}
+
+	public Date getDataRecebimentoEsperado() {
+		return dataRecebimentoEsperado;
+	}
+
+	public void setDataRecebimentoEsperado(Date dataRecebimentoEsperado) {
+		this.dataRecebimentoEsperado = dataRecebimentoEsperado;
+	}
+
+	public String getTipoReceita() {
+		return tipoReceita;
+	}
+
+	public void setTipoReceita(String tipoReceita) {
+		this.tipoReceita = tipoReceita;
+	}
+
+	public String getDescricaoReceita() {
+		return descricaoReceita;
+	}
+
+	public void setDescricaoReceita(String descricaoReceita) {
+		this.descricaoReceita = descricaoReceita;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+}
