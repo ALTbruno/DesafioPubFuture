81863f390f5d24913e1aade322175194cb8d59eb
Bruno Barbosa <brb.dev@outlook.com>
2022-01-04 20:04:25 -0400

    adiciona model despesa

---
+++ src/main/java/com/ALTbruno/DesafioPubFuture/model/Despesa.java
@@ -0,0 +1,85 @@
+package com.ALTbruno.DesafioPubFuture.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "TB_DESPESAS")
+public class Despesa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idDespesa;
+
+	@NotNull
+	@DecimalMin(value = "0")
+	private BigDecimal valorDespesa;
+
+	@NotNull
+	private Date dataPagamento;
+
+	@NotNull
+	private Date dataPagamentoEsperado;
+
+	@NotNull
+	@Size(max = 15)
+	private String tipoDespesa;
+
+	@ManyToOne
+	@JoinColumn(name = "id_conta")
+	private Conta conta;
+
+
+	public Integer getIdDespesa() {
+		return idDespesa;
+	}
+
+	public void setIdDespesa(Integer idDespesa) {
+		this.idDespesa = idDespesa;
+	}
+
+	public BigDecimal getValorDespesa() {
+		return valorDespesa;
+	}
+
+	public void setValorDespesa(BigDecimal valorDespesa) {
+		this.valorDespesa = valorDespesa;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public Date getDataPagamentoEsperado() {
+		return dataPagamentoEsperado;
+	}
+
+	public void setDataPagamentoEsperado(Date dataPagamentoEsperado) {
+		this.dataPagamentoEsperado = dataPagamentoEsperado;
+	}
+
+	public String getTipoDespesa() {
+		return tipoDespesa;
+	}
+
+	public void setTipoDespesa(String tipoDespesa) {
+		this.tipoDespesa = tipoDespesa;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+}
