310c1978c54bfe537302da73b428af4a1a88f3b3
Bruno Barbosa <brb.dev@outlook.com>
2022-01-05 20:59:33 -0400

    adiciona anotacao Repository em DespesaRepository

--- src/main/java/com/ALTbruno/DesafioPubFuture/repository/DespesaRepository.java
+++ src/main/java/com/ALTbruno/DesafioPubFuture/repository/DespesaRepository.java
@@ -2,6 +2,8 @@ package com.ALTbruno.DesafioPubFuture.repository;
 
 import com.ALTbruno.DesafioPubFuture.model.Despesa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DespesaRepository extends JpaRepository<Despesa, Integer> {
 }
