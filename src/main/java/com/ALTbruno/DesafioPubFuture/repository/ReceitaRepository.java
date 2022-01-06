ab33877fe4b3cd459f9782074b0c548a8cf62717
Bruno Barbosa <brb.dev@outlook.com>
2022-01-05 20:57:52 -0400

    adiciona anotacao repository em ReceitaRepository

--- src/main/java/com/ALTbruno/DesafioPubFuture/repository/ReceitaRepository.java
+++ src/main/java/com/ALTbruno/DesafioPubFuture/repository/ReceitaRepository.java
@@ -2,6 +2,8 @@ package com.ALTbruno.DesafioPubFuture.repository;
 
 import com.ALTbruno.DesafioPubFuture.model.Receita;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReceitaRepository extends JpaRepository<Receita, Integer> {
 }
