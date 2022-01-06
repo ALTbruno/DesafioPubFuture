9109a7a84010686bbb10aee166b8ca5b3f287964
Bruno Barbosa <brb.dev@outlook.com>
2022-01-05 20:58:26 -0400

    insere metodos http em ReceitaController

---
+++ src/main/java/com/ALTbruno/DesafioPubFuture/controller/ReceitaController.java
@@ -0,0 +1,82 @@
+package com.ALTbruno.DesafioPubFuture.controller;
+
+import com.ALTbruno.DesafioPubFuture.model.Receita;
+import com.ALTbruno.DesafioPubFuture.repository.ContaRepository;
+import com.ALTbruno.DesafioPubFuture.repository.ReceitaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/receitas")
+public class ReceitaController {
+
+	private ReceitaRepository receitaRepository;
+	private ContaRepository contaRepository;
+
+	@Autowired
+	ReceitaController(ReceitaRepository receitaRepository, ContaRepository contaRepository) {
+		this.receitaRepository = receitaRepository;
+		this.contaRepository = contaRepository;
+	}
+
+	@PostMapping
+	private ResponseEntity<Receita> adicionarReceita(@RequestBody Receita receita) {
+
+		ResponseEntity<Receita> response;
+		if(contaRepository.findById(receita.getConta().getIdConta()).isPresent()){
+			response = ResponseEntity.ok(receitaRepository.save(receita));
+		} else {
+			response =ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
+		return response;
+	}
+
+	@PutMapping("/{idReceita}")
+	private ResponseEntity<Receita> editarReceita(@PathVariable Integer idReceita, @RequestBody Receita receita) {
+
+		receita.setIdReceita(idReceita);
+
+		if(receita.getValorReceita() == null){
+			receita.setValorReceita(receitaRepository.findById(idReceita).get().getValorReceita());
+		}
+		if(receita.getDataRecebimento() == null){
+			receita.setDataRecebimento(receitaRepository.findById(idReceita).get().getDataRecebimento());
+		}
+		if(receita.getDataRecebimentoEsperado() == null){
+			receita.setDataRecebimentoEsperado(receitaRepository.findById(idReceita).get().getDataRecebimentoEsperado());
+		}
+		if(receita.getTipoReceita() == null){
+			receita.setTipoReceita(receitaRepository.findById(idReceita).get().getTipoReceita());
+		}
+		if(receita.getDescricaoReceita() == null){
+			receita.setDescricaoReceita(receitaRepository.findById(idReceita).get().getDescricaoReceita());
+		}
+		if(receita.getConta() == null){
+			receita.setConta(receitaRepository.findById(idReceita).get().getConta());
+		}
+
+		return ResponseEntity.ok(receitaRepository.save(receita));
+	}
+
+	@GetMapping
+	private ResponseEntity<List<Receita>> listarReceitas() {
+		return ResponseEntity.ok(receitaRepository.findAll());
+	}
+
+	@GetMapping("/{idReceita}")
+	private  ResponseEntity<Optional<Receita>> listarReceitaPeloId(@PathVariable Integer idReceita) {
+		return ResponseEntity.ok(receitaRepository.findById(idReceita));
+	}
+
+	@DeleteMapping("/{idReceita}")
+	private void removerReceitaPeloId(@PathVariable Integer idReceita) {
+		receitaRepository.deleteById(idReceita);
+	}
+
+}
