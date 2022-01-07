0e426fe90a549299bde9ef3bd686b45a4ebdcc1a
Bruno Barbosa <brb.dev@outlook.com>
2022-01-07 08:15:33 -0400

    atualiza getContas no Front-end

--- src/main/resources/static/js/get_contas.js
+++ src/main/resources/static/js/get_contas.js
@@ -1,27 +1,40 @@
 const listaContas = document.getElementById('listaContas');
 
+const url = 'http://localhost:8080/contas';
+
+let output = '';
+
+var renderContas = (contas) => {
+
+	contas.forEach(conta =>{
+		output += 
+					`
+					<div class="card" data-id=${conta.idConta} style="width: 18rem;">
+						<div class="card-header">Conta ${conta.instituicaoFinanceira.nomeInstituicaoFinanceira}</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">Agência: ${conta.agenciaConta}</li>
+							<li class="list-group-item">Número: ${conta.numeroConta}</li>
+							<li class="list-group-item">Tipo: ${conta.tipoConta}</li>
+							<li class="list-group-item">Saldo: ${conta.saldoConta}</li>
+						</ul>
+						<div class="btn-group" role="group" aria-label="Caixa com botões editar e apagar">
+							<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editarContaModal">Editar</button>
+							<button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#apagarContaModal">Apagar</button>
+						</div>
+					</div>
+					`;
+	});
+	listaContas.innerHTML = output;
+}
+
 async function getContas() {
 	try {
-		const response = await fetch('http://localhost:8080/contas');
+		const response = await fetch(url);
 		
 		const data = await response.json();
-		console.log(data);
 
-		data.map((conta) => {
+		renderContas(data);
 
-			const card = document.createElement('div'); card.classList.add('card'); card.style.width = '18rem';
-				const cardHeader = document.createElement('div'); cardHeader.classList.add('card-header'); cardHeader.innerText = `Conta ${conta.instituicaoFinanceira.nomeInstituicaoFinanceira}`; card.append(cardHeader);
-				const infosConta = document.createElement('ul'); infosConta.classList.add('list-group','list-group-flush'); card.append(infosConta);
-					const agencia = document.createElement('li'); agencia.classList.add('list-group-item'); agencia.innerText = `Agência: ${conta.agenciaConta}`; infosConta.append(agencia);
-					const numero = document.createElement('li'); numero.classList.add('list-group-item'); numero.innerText = `Número: ${conta.numeroConta}`; infosConta.append(numero);
-					const tipo = document.createElement('li'); tipo.classList.add('list-group-item'); tipo.innerText = `Tipo: ${conta.tipoConta}`; infosConta.append(tipo);
-					const saldo = document.createElement('li'); saldo.classList.add('list-group-item'); saldo.innerText = `Saldo: ${conta.saldoConta}`; infosConta.append(saldo);
-				const btnDiv = document.createElement('div'); btnDiv.classList.add('btn-group'); btnDiv.setAttribute('role', 'group'); btnDiv.setAttribute('aria-label', 'Basic mixed styles example'); card.append(btnDiv);
-					const btnEditar = document.createElement('button'); btnEditar.classList.add('btn', 'btn-primary'); btnEditar.innerText = 'Editar'; btnDiv.append(btnEditar);
-					const btnApagar = document.createElement('button'); btnApagar.classList.add('btn', 'btn-danger'); btnApagar.innerText = 'Apagar'; btnDiv.append(btnApagar);
-
-			  listaContas.append(card)
-		})
 	} catch (error) {
 		console.log(error);
 	}
